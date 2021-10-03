27e9559abf0e2bef20d7483a295bce43d161d3e4
abdulqadir18 <boxwalaabdulqadir2002@gmail.com>
2021-10-03 11:09:07 +0530

    initial commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerScript playerScript;
+    public Score score;
+    public GameController gameController;
+    public AudioSource audioSource1;
+    public AudioSource audioSource2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Collectables")
+        {
+            audioSource1.Play();
+            score.AddScore(1);
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Obstacles")
+        {
+            audioSource2.Play();
+            gameController.GameOver();
+            playerScript.enabled = false;
+        }
+    }
+}
